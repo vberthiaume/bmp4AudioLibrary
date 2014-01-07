7181368c712b2c8a1a3d7ab384692c39e1e4e505
vberthiaume <vberthiaume@gmail.com>
2014-01-07 17:57:53 -0500

    commit

--- bmp4AudioLibrary.h
+++ bmp4AudioLibrary.h
@@ -11,11 +11,16 @@
 class bmp4
 {
 public:
-	template <class T> T sign(T &v);
+
+	//template function, returns 1 for positive numbers and -1 for negative numbers
+	template <class T> static T sign(T &v);
 
 	//template function used to apply overdrive on single channels
 	template<class T> static void overdriveSingleChannel(T &p_in, T &p_out);
+
+	//template function used to apply distortion on single channels
 	template<class T> static void distortionSingleChannel(T &p_in, T &p_out);
 	
+	//template function used to bypass single channels, can be used for testing
 	template<class T> static void bypassSingleChannel(T &p_in, T &p_out);
 };
